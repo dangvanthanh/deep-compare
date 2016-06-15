86162c172507e20f710367b0a711d8c76f83e00a
Dang Van Thanh <dangvanthanh@dangthanh.org>
2016-06-15 21:56:25 +0700

    Use standard.js instead of xo

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-'use strict';
+'use strict'
 
 module.exports = function (o1, o2) {
-	if (typeof o1 !== 'object' || typeof o2 !== 'object') {
-		return false;
-	}
+  if (typeof o1 !== 'object' || typeof o2 !== 'object') {
+    return false
+  }
 
-	var o1Props = Object.getOwnPropertyNames(o1);
-	var o2Props = Object.getOwnPropertyNames(o2);
+  var o1Props = Object.getOwnPropertyNames(o1)
+  var o2Props = Object.getOwnPropertyNames(o2)
 
-	if (o1Props.length !== o2Props.length) {
-		return false;
-	}
+  if (o1Props.length !== o2Props.length) {
+    return false
+  }
 
-	for (var i = 0, len = o1Props.length; i < len; i++) {
-		var propName = o1Props[i];
+  for (var i = 0, len = o1Props.length; i < len; i++) {
+    var propName = o1Props[i]
 
-		if (o1[propName] !== o2[propName]) {
-			return false;
-		}
-	}
+    if (o1[propName] !== o2[propName]) {
+      return false
+    }
+  }
 
-	return true;
-};
+  return true
+}
